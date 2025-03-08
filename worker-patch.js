5004980803d3419e654d6636f938aca6055f2045
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2025-03-07 20:41:34 -0600

    Update worker-patch.js

--- worker-patch.js
+++ worker-patch.js
@@ -1,1 +1,19 @@
 
+  (() => {
+    const _Worker = globalThis.Worker;
+    const $Worker = class Worker extends _Worker {
+      constructor(url,options) {
+        const patch = 'console.log("asdf");';
+        const request = new XMLHttpRequest();
+        request.open("GET", url, false);
+        request.send();
+
+if (request.status === 200) {
+  console.log(request.responseText);
+}
+
+      }
+    };
+    globalThis.Worker = $Worker;
+  })();
+})();
