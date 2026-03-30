2ad01885558f5aaa0fbcfa0d924880046f90cfe6
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-03-30 12:38:21 -0500

    style: js-beautify formatting

--- worker-patch.js
+++ worker-patch.js
@@ -1,16 +1,18 @@
 (() => {
-    const _Worker = globalThis.Worker;
-    const $Worker = class Worker extends _Worker {
-      constructor(url,options) {
-        const patch = 'console.log("asdf");';
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", url, false);
-        xhr.send();
-        const code = patch + xhr.responseText;
-        const newURL = URL.createObjectURL(new Blob([code],{type:"text/javascript"}));
-        super(newURL,options);
-      }
-    };
-    globalThis.Worker = $Worker;
-  })();
+  const _Worker = globalThis.Worker;
+  const $Worker = class Worker extends _Worker {
+    constructor(url, options) {
+      const patch = 'console.log("asdf");';
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", url, false);
+      xhr.send();
+      const code = patch + xhr.responseText;
+      const newURL = URL.createObjectURL(new Blob([code], {
+        type: "text/javascript"
+      }));
+      super(newURL, options);
+    }
+  };
+  globalThis.Worker = $Worker;
+})();
 })();
